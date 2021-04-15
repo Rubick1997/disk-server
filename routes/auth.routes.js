d40083e3ae91fd457dbfaf1d04a7049d7641d75d
Rustam <rustam905@gmail.com>
2021-04-14 19:03:22 -0700

    registration and validation

---
+++ routes/auth.routes.js
@@ -0,0 +1,41 @@
+const Router = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const router = new Router();
+const { check, validationResult } = require("express-validator");
+
+router.post(
+	"/registration",
+	[
+		check("email", "Incorrect mail").isEmail(),
+		check(
+			"password",
+			"Password should be longer than 3 and shorter than 12"
+		).isLength({ min: 3, max: 12 }),
+	],
+	async (req, res) => {
+		try {
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({message:"Uncorrect request",errors})
+            }
+            
+			const { email, password } = req.body;
+
+			const candidate = await User.findOne({ email });
+			if (candidate) {
+				return res
+					.status(400)
+					.json({ message: `User with email ${email} already exists` });
+			}
+			const hashPassword = await bcrypt.hash(password, 15);
+			const user = new User({ email, password: hashPassword });
+			await user.save();
+			return res.json({ message: "User was created" });
+		} catch (e) {
+			console.log(e), res.sendz({ message: "Server error" });
+		}
+	}
+);
+
+module.exports = router
